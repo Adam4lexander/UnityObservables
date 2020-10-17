836f5d967e31cce34965a535b757d52e8880d614
atwite <twite.adam@gmail.com>
2020-10-17 13:08:29 +1100

    initial commit

---
+++ Assets/UnityObservables/Observable.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityObservables {
+
+    [Serializable]
+    public class ObservableBool : Observable<bool> { }
+
+    [Serializable]
+    public class ObservableFloat : Observable<float> { }
+
+    [Serializable]
+    public class Observable<T> : ObservableBase, IEquatable<Observable<T>> {
+
+        [SerializeField]
+        T value;
+
+        T prevValue;
+        bool prevValueInitialized = false;
+
+        public override event Action OnChanged;
+        public event Action<T, T> OnChangedValues;
+
+        protected override string ValuePropName { get { return "value"; } }
+
+        public Observable() {
+        }
+
+        public Observable(T value) {
+            this.value = value;
+        }
+
+        public T Value {
+            get { return value; }
+            set {
+                if (value is UnityEngine.Object || this.value is UnityEngine.Object) {
+                    if (ReferenceEquals(value, this.value)) {
+                        return;
+                    }
+                } else if ((value == null && this.value == null) || value != null && value.Equals(this.value)) {
+                    return;
+                }
+
+                prevValue = value;
+                prevValueInitialized = true;
+                var storePrev = this.value;
+                this.value = value;
+                if (OnChanged != null) {
+                    OnChanged();
+                }
+                if (OnChangedValues != null) {
+                    OnChangedValues(storePrev, value);
+                }
+            }
+        }
+
+        public static explicit operator T(Observable<T> observable) {
+            return observable.value;
+        }
+
+        public override string ToString() {
+            return value.ToString();
+        }
+
+        public bool Equals(Observable<T> other) {
+            if (other == null) {
+                return false;
+            }
+            return other.value.Equals(value);
+        }
+
+        public override bool Equals(object other) {
+            return other != null
+                && other is Observable<T>
+                && ((Observable<T>)other).value.Equals(value);
+        }
+
+        public override int GetHashCode() {
+            return value.GetHashCode();
+        }
+
+        protected override void OnBeginGui() {
+            prevValue = value;
+            prevValueInitialized = true;
+        }
+
+        public override void OnValidate() {
+            if (prevValueInitialized) {
+                var nextValue = value;
+                value = prevValue;
+                Value = nextValue;
+            } else {
+                prevValue = value;
+                prevValueInitialized = true;
+            }
+        }
+    }
+}
---
+++ Assets/UnityObservables/ObservableBase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using System.Linq;
+using System.Reflection;
+#endif
+
+namespace UnityObservables {
+
+    public abstract class ObservableBase {
+        public abstract event Action OnChanged;
+        public abstract void OnValidate();
+        protected abstract string ValuePropName { get; }
+        protected abstract void OnBeginGui();
+
+#if UNITY_EDITOR
+        [CustomPropertyDrawer(typeof(ObservableBase), true)]
+        public class ObservableDrawer : PropertyDrawer {
+            public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+
+                var obs = fieldInfo.GetValue(GetParent(property)) as ObservableBase;
+
+                obs.OnBeginGui();
+
+                var val = property.FindPropertyRelative(obs.ValuePropName);
+
+                EditorGUI.BeginChangeCheck();
+
+                EditorGUI.PropertyField(position, val, label, true);
+
+                if (EditorGUI.EndChangeCheck()) {
+                    property.serializedObject.ApplyModifiedProperties();
+                    obs.OnValidate();
+                }
+            }
+
+            public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+                var obs = fieldInfo.GetValue(GetParent(property)) as ObservableBase;
+                SerializedProperty val = property.FindPropertyRelative(obs.ValuePropName);
+                return EditorGUI.GetPropertyHeight(val, label, true);
+            }
+
+            public static object GetParent(SerializedProperty prop) {
+                var path = prop.propertyPath.Replace(".Array.data[", "[");
+                object obj = prop.serializedObject.targetObject;
+                var elements = path.Split('.');
+                foreach (var element in elements.Take(elements.Length - 1)) {
+                    if (element.Contains("[")) {
+                        var elementName = element.Substring(0, element.IndexOf("["));
+                        var index = Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
+                        obj = GetValue(obj, elementName, index);
+                    } else {
+                        obj = GetValue(obj, element);
+                    }
+                }
+                return obj;
+            }
+
+            static object GetValue(object source, string name) {
+                if (source == null)
+                    return null;
+                var type = source.GetType();
+                var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                if (f == null) {
+                    var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                    if (p == null)
+                        return null;
+                    return p.GetValue(source, null);
+                }
+                return f.GetValue(source);
+            }
+
+            static object GetValue(object source, string name, int index) {
+                var enumerable = GetValue(source, name) as IEnumerable;
+                var enm = enumerable.GetEnumerator();
+                while (index-- >= 0)
+                    enm.MoveNext();
+                return enm.Current;
+            }
+        }
+    }
+#endif
+}
